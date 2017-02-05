267c5b0f9b81861ffb3d630d789b2b26ba8ff9b0
Will James <wjames@thoughtworks.com>
2017-02-05 21:07:42 +1100

    remove aws cli thing

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,5 @@ npm prune --production
 ./decrypt.sh
 source ./config/env.sh
 unset AWS_SESSION_TOKEN
-./install_aws_cli.sh
-/tmp/lambci/home/bin/aws cloudformation list-stacks --region ap-southeast-2
 
 SLS_DEBUG=* node_modules/.bin/serverless deploy --stage=dev --verbose
