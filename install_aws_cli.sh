d90e7347298232c32aac9682931356e587f25e92
Will James <wjames@thoughtworks.com>
2017-02-05 20:07:17 +1100

    ugh.

--- install_aws_cli.sh
+++ install_aws_cli.sh
@@ -1,4 +1,4 @@
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 ./awscli-bundle/install -b ~/bin/aws
-export PATH=~/bin:$PATH
+export PATH=/tmp/lambci/home/bin/:$PATH
