bb7851390ce0dc5f9134ffb9a9d78396a1c0b4f4
Will James <wjames@thoughtworks.com>
2017-02-05 20:10:05 +1100

    one more

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,7 @@ npm prune --production
 ./decrypt.sh
 source config/env.sh
 ./install_aws_cli.sh
-aws cloudformation list-stacks --region ap-southeast-2
+/tmp/lambci/home/bin/aws cloudformation list-stacks --region ap-southeast-2
 
-AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
-AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
-AWS_PROFILE=serverless \
 SLS_DEBUG=* \
 node_modules/.bin/serverless deploy --stage=dev --verbose
