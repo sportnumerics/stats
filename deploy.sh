7f9bf7f9ed37c24444f9a49b2349ffa4c265c1a4
Will James <wjames@thoughtworks.com>
2017-02-05 18:55:41 +1100

    Remove some unessential stuff. Still confused about why i am getting a token error.

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,4 @@ npm prune --production
 
 ./decrypt.sh
 source config/env.sh
-aws cloudformation describe-stacks --region ap-southeast-2 --stack-name sportnumerics-dev
-SLS_DEBUG=* node_modules/.bin/serverless deploy --stage=dev --verbose
+SLS_DEBUG=* node_modules/.bin/serverless --verbose
