f207a177b78acf242a81bf941ce7f5ee8a785d66
Will James <wjames@thoughtworks.com>
2017-02-05 19:07:21 +1100

    add region to see if that might be required.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ npm prune --production
 
 ./decrypt.sh
 source config/env.sh
-SLS_DEBUG=* node_modules/.bin/serverless deploy --stage=dev --verbose
+SLS_DEBUG=* node_modules/.bin/serverless deploy --stage=dev --region=ap-southeast-2 --verbose
