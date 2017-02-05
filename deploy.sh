d17957a8879fed0e93baa2021fde7b4d948e7149
Will James <wjames@thoughtworks.com>
2017-02-05 19:54:07 +1100

    try again to print some cf info to test credentials.

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,9 @@ npm prune --production
 
 ./decrypt.sh
 source config/env.sh
+sudo pip install awscli
+aws cloudformation list-stacks --region ap-southeast-2
+
 AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
 AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
 AWS_PROFILE=serverless \
