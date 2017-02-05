5606b28e6f688c2c18a079c86131ad9f4592c19e
Will James <wjames@thoughtworks.com>
2017-02-05 18:59:31 +1100

    do need stage. still no solution

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ npm prune --production
 
 ./decrypt.sh
 source config/env.sh
-SLS_DEBUG=* node_modules/.bin/serverless deploy --verbose
+SLS_DEBUG=* node_modules/.bin/serverless deploy --stage=dev --verbose
