b9a13361f617df4871cd1f2fae1c3c3b846600ae
Will James <wjames@thoughtworks.com>
2017-02-05 21:06:13 +1100

    Try unsetting AWS_SESSION_TOKEN since it was set. This may actually work.

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,7 @@ npm prune --production
 
 ./decrypt.sh
 source ./config/env.sh
-echo $AWS_ACCESS_KEY_ID
-echo $AWS_SECRET_ACCESS_KEY
-echo $AWS_SESSION_TOKEN
+unset AWS_SESSION_TOKEN
 ./install_aws_cli.sh
 /tmp/lambci/home/bin/aws cloudformation list-stacks --region ap-southeast-2
 
