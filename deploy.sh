5a459b5caf99b50e9ad06051c5adda46e5898a31
Will James <wjames@thoughtworks.com>
2017-02-05 18:48:52 +1100

    try to describe stacks from deployment to see whats up.

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,5 @@ npm prune --production
 
 ./decrypt.sh
 source config/env.sh
+aws cloudformation describe-stacks --region ap-southeast-2 --stack-name sportnumerics-dev
 SLS_DEBUG=* node_modules/.bin/serverless deploy --stage=dev --verbose
