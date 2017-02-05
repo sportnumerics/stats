7cc18cd1eeb2cba66cd8335c9fbfb466dd3854ac
Will James <wjames@thoughtworks.com>
2017-02-05 21:02:36 +1100

    echo keys to see wtf.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 npm prune --production
 
 ./decrypt.sh
-source config/env.sh
+source ./config/env.sh
+echo $AWS_ACCESS_KEY_ID
+echo $AWS_SECRET_ACCESS_KEY
+echo $AWS_SESSION_TOKEN
 ./install_aws_cli.sh
 /tmp/lambci/home/bin/aws cloudformation list-stacks --region ap-southeast-2
 
-SLS_DEBUG=* \
-node_modules/.bin/serverless deploy --stage=dev --verbose
+SLS_DEBUG=* node_modules/.bin/serverless deploy --stage=dev --verbose
