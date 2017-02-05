dc9351204be1d302d27561a790af27570a631ec7
Will James <wjames@thoughtworks.com>
2017-02-05 19:44:54 +1100

    Try to fix by explicitly putting credentials in command line. Unlikely to work.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 npm prune --production
 
 ./decrypt.sh
-# source config/env.sh
-SLS_DEBUG=* node_modules/.bin/serverless deploy --stage=dev --region=ap-southeast-2 --verbose
+source config/env.sh
+AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID \
+AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+AWS_PROFILE=serverless \
+SLS_DEBUG=* \
+node_modules/.bin/serverless deploy --stage=dev --verbose
