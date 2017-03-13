1550676e7aa4a30370f0e546b993987e392ca9a3
Will James <wjames@thoughtworks.com>
2017-03-13 15:23:15 +1100

    Better naming consistency and add game by game service test.

---
+++ test/service/game-by-game-tests.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const nock = require('nock');
+const fixtures = require('../fixtures');
+const teamsService = require('../../lib/service/teams');
+const gameByGameService = require('../../lib/service/game-by-game');
+
+describe('game-by-game-service', () => {
+  describe('getHTMLFromNcaa', () => {
+    beforeEach(() => {
+      nock('http://stats.ncaa.org/').get(/\/player\/game_by_game.*/).reply(200, fixtures.gameByGame);
+    });
+
+    it('should return an html document', () => {
+      let htmlPromise = gameByGameService.getHtmlFromNcaa('2016', 721);
+
+      return expect(htmlPromise).to.eventually.be.a('string');
+    });
+  });
+});
