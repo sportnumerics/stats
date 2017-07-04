cfe5a5a326a4adc7444f272839be692e367a7af2
Will James <wjames@thoughtworks.com>
2017-07-04 13:25:49 +1000

    Add some error handling

--- test/service/queue-tests.js
+++ test/service/queue-tests.js
@@ -21,7 +21,7 @@ describe('queue-service', () => {
       var mockSqs = sinon.mock(sqs)
         .expects('sendMessageBatchAsync')
         .withArgs(expectedParams)
-        .returns(Promise.resolve());
+        .returns(Promise.resolve(batchSuccessObject(1)));
 
       let messages = [{
         key: 'value'
@@ -36,8 +36,10 @@ describe('queue-service', () => {
     it('should chunk the messages into batches of ten', () => {
       var mockSqs = sinon.mock(sqs)
         .expects('sendMessageBatchAsync')
-        .exactly(2)
-        .returns(Promise.resolve());
+        .exactly(2);
+        
+      mockSqs.onFirstCall().returns(Promise.resolve(batchSuccessObject(10)))
+        .onSecondCall().returns(Promise.resolve(batchSuccessObject(5)));
 
       let messagePrototype = { key: 'value' };
       let messages = _.fill(Array(15), messagePrototype);
@@ -48,7 +50,7 @@ describe('queue-service', () => {
       });
     });
 
-    it('should return an error if any of the messages fail to send', () => {
+    it('should return an error if the message post request fails', () => {
       var mockSqs = sinon.mock(sqs)
         .expects('sendMessageBatchAsync')
         .exactly(1)
@@ -58,11 +60,53 @@ describe('queue-service', () => {
 
       let promise = queue.sendMessages(messages);
 
-      return expect(promise).to.be.rejectedWith('Unable to send message to SQS.');
-    })
+      return expect(promise).to.be.rejectedWith('Unable to send message to SQS.')
+        .then(() => {
+          sqs.sendMessageBatchAsync.restore();
+        });
+    });
+
+    it('should return an error if any of the messages fail to send', () => {
+      var mockSqs = sinon.mock(sqs)
+        .expects('sendMessageBatchAsync')
+        .exactly(1)
+        .returns(Promise.resolve(batchFailedObject(1)));
+
+      let messages = [{ key: 'value' }];
+
+      let promise = queue.sendMessages(messages);
+
+      return expect(promise).to.be.rejectedWith('Unable to send message to SQS.')
+        .then(() => {
+          sqs.sendMessageBatchAsync.restore();
+        });
+    });
   });
 
   describe('receiveMessage', () => {
 
   });
 });
+
+function batchSuccessObject(count) {
+  return {
+    Successful: _.fill(Array(count), {
+      Id: 'mock-id',
+      MessageId: 'mock-message-id',
+      MD5OfMessageBody: 'mock-md5'
+    }),
+    Failed: []
+  };
+}
+
+function batchFailedObject(count) {
+  return {
+    Successful: [],
+    Failed: _.fill(Array(count), {
+        Id: 'mock-id',
+        SenderFault: true,
+        Code: '403',
+        Message: 'you suck'
+      })
+  }
+}
