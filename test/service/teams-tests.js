82ae889954fa61a814b50d3636389695dbbe978c
Will James <wjames@thoughtworks.com>
2018-02-07 14:48:07 -0600

    Add a timeout to collection

--- test/service/teams-tests.js
+++ test/service/teams-tests.js
@@ -2,18 +2,38 @@
 
 let nock = require('nock'),
   fixtures = require('../fixtures'),
-  teamsService = require('../../lib/service/teams');
+  teamsService = require('../../lib/service/teams'),
+  sinon = require('sinon');
 
 describe('teams-service', () => {
   describe('getHtmlFromNcaa', () => {
-    beforeEach(() => {
-      nock('http://stats.ncaa.org').get(/\/team\/inst_team_list.*/).reply(200, fixtures.teamList);
-    });
+    describe('when things go well', () => {
+      beforeEach(() => {
+        nock('http://stats.ncaa.org').get(/\/team\/inst_team_list.*/).reply(200, fixtures.teamList);
+      });
 
-    it('should return an html document', () => {
-      let htmlPromise = teamsService.getHtmlFromNcaa('2016', fixtures.womensDivision1);
+      it('should return an html document', () => {
+        let htmlPromise = teamsService.getHtmlFromNcaa('2016', fixtures.womensDivision1);
 
-      return expect(htmlPromise).to.eventually.be.a('string');
+        return expect(htmlPromise).to.eventually.be.a('string');
+      });
+    })
+
+    describe('when things dont go well', () => {
+      it('should timeout in  seconds if the response is not returned in time', () => {
+        let clock = sinon.useFakeTimers();
+
+        nock('http://stats.ncaa.org')
+          .get(/\/team\/inst_team_list.*/)
+          .delayConnection(20000)
+          .reply(200, fixtures.teamList);
+
+        let htmlPromise = teamsService.getHtmlFromNcaa('2016', fixtures.womensDivision1);
+
+        clock.tick(20000);
+
+        return expect(htmlPromise).to.eventually.be.rejected;
+      });
     });
   });
 });
