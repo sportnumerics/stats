c5d14c0959c577f5a22f2ad12c76dde1645f92a8
Will James <wjames@thoughtworks.com>
2018-03-11 19:33:10 -0500

    Move bootstrap to separate file, although still inline in template via serverless

---
+++ bootstrap.py
@@ -0,0 +1,37 @@
+#pylint: disable=E0401
+import boto3
+import os
+
+
+def handler(event, context):
+    client = boto3.client('ecs')
+    response = client.run_task(
+        cluster=os.environ['ECS_CLUSTER'],
+        launchType='FARGATE',
+        taskDefinition=os.environ['ECS_TASK'],
+        count=1,
+        platformVersion='LATEST',
+        networkConfiguration={
+            'awsvpcConfiguration': {
+                'subnets': [
+                    'subnet-53f5bb0a',
+                    'subnet-24bfda41'
+                ],
+                'assignPublicIp': 'ENABLED'
+            }
+        },
+        overrides={
+            'containerOverrides': [
+                {
+                    'name': 'stats',
+                    'environment': [
+                        {
+                            'name': 'YEAR',
+                            'value': event['year']
+                        }
+                    ]
+                }
+            ]
+        }
+    )
+    return str(response)
