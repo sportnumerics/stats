19a90d3de4f87f4d6882478b64acf37cdd447c16
Will James <wjames@thoughtworks.com>
2018-04-04 19:34:44 -0400

    Put bootstrap in separate file. As a result need new deployment steps to packge and upload to s3

--- deploy.sh
+++ deploy.sh
@@ -27,12 +27,36 @@ docker build -t $IMAGE_NAME .
 
 STACK_NAME="sportnumerics-stats-$STAGE"
 
+DEPLOYMENT_BUCKET=$(
+  aws cloudformation describe-stacks \
+    --stack-name $STACK_NAME \
+    --query 'Stacks[0].Outputs[?OutputKey==`DeploymentBucket`].OutputValue' \
+    --output text
+  )
+
+if [ -z "$DEPLOYMENT_BUCKET" ]; then
+  TEMPORARY_BUCKET_NAME="temporary-sportnumerics-stats-deployment-bucket"
+  aws s3 mb "s3://$TEMPORARY_BUCKET_NAME" --region $AWS_DEFAULT_REGION
+  DEPLOYMENT_BUCKET=$TEMPORARY_BUCKET_NAME
+fi
+
+mkdir -p build
+
+aws cloudformation package \
+  --template-file cloudformation-update.yml \
+  --s3-bucket $DEPLOYMENT_BUCKET \
+  --output-template-file build/cf.yml
+
 aws cloudformation deploy \
   --stack-name=$STACK_NAME \
-  --template-file cloudformation.yml \
+  --template-file build/cf.yml \
   --parameter-overrides Stage=$STAGE \
   --capabilities CAPABILITY_IAM \
   --no-fail-on-empty-changeset
 
 docker tag $IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_NAME:latest
 docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_NAME:latest
+
+if [ ! -z "$TEMPORARY_BUCKET_NAME" ]; then
+  aws s3 rb "s3://$TEMPORARY_BUCKET_NAME"
+fi
